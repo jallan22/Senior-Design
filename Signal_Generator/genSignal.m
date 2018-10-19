50c37473375eda733e10bfa4bc9e5dd4e43c930a
Peyton McClintock <peyton11@vt.edu>
2018-10-19 09:38:28 -0400

    Base Signal Generation Code

---
+++ Signal_Generator/genSignal.m
@@ -0,0 +1,25 @@
+% genSignal.m
+%
+% DESCRIPTION: Short description of script purpose
+%
+% EXECUTION:
+%   [out1,out2] = filename(in1,in2,in3)
+%
+% INPUT:
+%   in1     [str]    = variable description
+%   in2     [1x1]    = variable description
+%   in3     [struct] = variable description
+%       in3.field1 [1x1] = variable description
+%       in3.field2 [1x1] = variable description
+%
+% OUTPUT:
+%   out1    [2x2]   = variable description
+%   out2    [str]   = variable description (optional)
+%
+% EDC Systems - ECE 4805: Senior Design
+%
+% Origional Version [10/19/2018], Peyton McClintock
+
+function [] = genSignal()
+
+fprintf('\nSignal Generated\n')
---
+++ Signal_Generator/signalTest.m
@@ -0,0 +1,25 @@
+% signalTest.m
+%
+% DESCRIPTION: Short description of script purpose
+%
+% EXECUTION:
+%   [out1,out2] = filename(in1,in2,in3)
+%
+% INPUT:
+%   in1     [str]    = variable description
+%   in2     [1x1]    = variable description
+%   in3     [struct] = variable description
+%       in3.field1 [1x1] = variable description
+%       in3.field2 [1x1] = variable description
+%
+% OUTPUT:
+%   out1    [2x2]   = variable description
+%   out2    [str]   = variable description (optional)
+%
+% EDC Systems - ECE 4805: Senior Design
+%
+% Origional Version [mm/dd/yyyy], Peyton McClintock
+
+clear
+
+genSignal()
