45ce4c1efc0598c9e15327b0f045d205731c5e3b
Peyton McClintock <peyton11@vt.edu>
2018-10-19 10:16:38 -0400

    Better Signal Generation Code

--- Signal_Generator/genSignal.m
+++ Signal_Generator/genSignal.m
@@ -20,6 +20,47 @@
 %
 % Origional Version [10/19/2018], Peyton McClintock
 
-function [] = genSignal()
+function [signal,sigTime] = genSignal(Ts,Fs,pulse)
 
-fprintf('\nSignal Generated\n')
+%% Check Input
+
+% Do later
+
+%% Determine Group
+sinGroup = strcmpi(pulse.type,{'sin','cos'}); % Sinusoids
+
+%% Generate Waveform
+
+% Sinusoid
+if any(sinGroup)
+    % Define Waveform
+    if sinGroup(1)
+        wavType = @sind;
+    else
+        wavType = @cosd;
+    end
+    pulseTime = 0:(1/Fs):pulse.duration-(1/Fs);
+    waveform = pulse.Amplitude*wavType(2*pi*pulse.freq*pulseTime + pulse.phase);
+    
+end
+   
+%% Generate Singal
+sigTime  = 0:(1/Fs):Ts-(1/Fs);
+signal   = zeros(1,length(sigTime));
+wavStart = pulse.startTime*Fs+1;
+wavEnd   = wavStart + pulse.duration*Fs -1;
+signal(wavStart:wavEnd) = waveform;
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- Signal_Generator/signalTest.m
+++ Signal_Generator/signalTest.m
@@ -1,25 +1,49 @@
 % signalTest.m
 %
-% DESCRIPTION: Short description of script purpose
-%
-% EXECUTION:
-%   [out1,out2] = filename(in1,in2,in3)
-%
-% INPUT:
-%   in1     [str]    = variable description
-%   in2     [1x1]    = variable description
-%   in3     [struct] = variable description
-%       in3.field1 [1x1] = variable description
-%       in3.field2 [1x1] = variable description
-%
-% OUTPUT:
-%   out1    [2x2]   = variable description
-%   out2    [str]   = variable description (optional)
+% DESCRIPTION: Test file for genSignal.m
 %
 % EDC Systems - ECE 4805: Senior Design
 %
-% Origional Version [mm/dd/yyyy], Peyton McClintock
+% Origional Version [10/19/2019], Peyton McClintock
 
 clear
 
-genSignal()
+%% Define Parameters
+
+% Enviornment
+Ts = 2; % s
+Fs = 1e3; % Hz
+% ADD NOISE ???
+
+% Signal
+pulse.type      = 'sin'; % Will always assume arguments are degrees
+pulse.freq      = 100;
+pulse.phase     = 0; % deg
+pulse.Amplitude = 1; % ???
+pulse.startTime = 1; % s
+pulse.duration  = 1; % s
+
+
+%% Generate Signal
+[signal,sigTime] = genSignal(Ts,Fs,pulse);
+
+%% Plot Signal
+figure(1); clf; hold on; grid on;
+plot(sigTime,signal)
+xlabel('Time (s)')
+ylabel('Amplitude')
+title('Test Signal')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
